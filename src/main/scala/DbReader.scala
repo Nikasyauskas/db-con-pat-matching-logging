1eddcbb0a1a153cdfd786188ffb7a7a924e337cf
Nikasyauskas <nikasyauskas@gmail.com>
2022-08-07 22:40:01 +0300

    exception if incorrect table

--- src/main/scala/DbReader.scala
+++ src/main/scala/DbReader.scala
@@ -3,13 +3,14 @@ package DbReader
 object DbReader {
 
   def readTable(tableName: String): Unit = tableName match {
-    case "books" => println("booksssss")
-    case "authors" => println("suthorsssss")
-    case _ =>
+    case "books" => println("<<< books >>>")
+    case "authors" => println("<<< authors >>>")
+    case _ => throw new IllegalArgumentException(s"can't resolve table ${tableName}")
   }
 
   def main(args: Array[String]): Unit = {
 
     readTable("authors")
+    readTable("actors")
   }
 }
