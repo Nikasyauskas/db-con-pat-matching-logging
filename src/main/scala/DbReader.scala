b8f4fecc21619fc736d4f1f5b2a391bcb9e348a7
Nikasyauskas <nikasyauskas@gmail.com>
2022-08-10 22:41:59 +0300

    add dimple logging

--- src/main/scala/DbReader.scala
+++ src/main/scala/DbReader.scala
@@ -1,16 +1,19 @@
 package DbReader
 
+import java.util.logging.Logger
+
 object DbReader {
 
+  val log = Logger.getLogger("DbReader")
   def readTable(tableName: String): Unit = tableName match {
-    case "books" => println("<<< books >>>")
-    case "authors" => println("<<< authors >>>")
+    case "books" => log.info("get books")
+    case "authors" => log.info("get authors")
     case _ => throw new IllegalArgumentException(s"can't resolve table ${tableName}")
   }
 
   def main(args: Array[String]): Unit = {
 
+    readTable("books")
     readTable("authors")
-    readTable("actors")
   }
 }
